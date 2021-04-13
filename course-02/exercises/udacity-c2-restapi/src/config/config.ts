c79682eb5a005cc73564f7dc114fd114b21025db
Mauricio Maia <mauricio@mpmaia.com.br>
2021-04-13 20:06:38 -0300

    First commit

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "teste",
+    "database": "udagram",
+    "host": "localhost",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "udagram",
+    "aws_media_bucket": "mpmaia-udacity-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
